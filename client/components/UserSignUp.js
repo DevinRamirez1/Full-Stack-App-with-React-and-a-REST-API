7a365c77dfd5909d5bd1cb59b0b7e4fca4d1aefa
DevinRamirez1 <devinramirez207@gmail.com>
2022-01-27 17:51:35 -0600

    Create UserSignUp.js

---
+++ client/components/UserSignUp.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function UserSignUp() {
+    return (
+        <main>
+            <div className="form--centered">
+                <h2>Sign Up</h2>
+
+                <form>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                    />
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        id="lastName"
+                        name="lastName"
+                        type="text"
+                    />
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input
+                        id="emailAddress"
+                        name="emailAddress"
+                        type="email"
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                    />
+                    <button className="button" type="submit">Sign Up</button><button className="button button--secondary" onClick={}>Cancel</button>
+                </form>
+                <p>Already have a user account? Click here to <a href="/signup">sign up</a>!</p>
+            </div>
+        </main>
+    )
+}
+
+export default UserSignUp;
