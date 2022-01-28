f17946fc647e07b9ea1b078728f215be9734e469
DevinRamirez1 <devinramirez207@gmail.com>
2022-01-28 16:56:03 -0600

    Create Header.js

---
+++ client/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <Header>
+            <div className="wrap header--flex">
+                <h1 className="header--logo"><a href="">Courses</a></h1>
+                <nav>
+                    <ul className="header--signedIn">
+                        <li>Welcome,</li>
+                        <li><a href="">Sign Out</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </Header>
+    )
+}
