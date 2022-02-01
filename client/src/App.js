cfa609ae04d32b04d8df7c4da3a94c76a99ba96c
DevinRamirez1 <devinramirez207@gmail.com>
2022-02-01 05:10:38 -0600

    Update App.js
    built out the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Header from './components/Header';
+import Courses from './components/Courses';
+import CourseDetail from './components/CourseDetail';
+import CreateCourse from './components/CreateCoures';
+import UpdateCourse from './components/UpdateCourse';
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <Header />
+      <Routes>
+        <Route exact path='/' element={<Courses />}/>
+        <Route path='/courses/:id' element={<CourseDetail />}/>
+        <Route path='/courses/create' element={<CreateCourse />}/>
+        <Route path='/courses/:id/update' element={<UpdateCourse />}/>
+        <Route path='/signin' element={<UserSignIn />}/>
+        <Route path='signout' element={<UserSignOut/>}/>
+        <Route path ='/signup' element={<UserSignUp/>}/>
+      </Routes>
+    </React.Fragment>
   );
 }
 
