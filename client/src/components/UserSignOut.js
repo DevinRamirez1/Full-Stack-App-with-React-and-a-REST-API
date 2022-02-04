60b8be7e1237565e685f9bb7bdbe501e6ed96121
DevinRamirez1 <devinramirez207@gmail.com>
2022-02-04 16:30:59 -0600

    Update UserSignOut.js

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -1,9 +1,9 @@
 import React, {useEffect} from "react";
-import {Redirect} from 'react-router-dom';
+import {Navigate} from 'react-router-dom';
 
 export default ({ context }) => {
     useEffect(() => context.actions.signout());
     return(
-        <Redirect to='/' />
+        <Navigate to='/' />
     );
 }
