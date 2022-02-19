1f7572de1a4061f00ac29de8850d373e6c519d66
DevinRamirez1 <devinramirez207@gmail.com>
2022-02-19 10:12:42 -0600

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,31 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import Form from './Form'
 
-function UserSignIn() {
+export default function UserSignIn({ context }) {
+        const [emailAddress, setEmailAddress] = useState('');
+        const [password, setPassword] = useState('');
+        const [errors, setErrors] = useState([]);
+
+        const navigate = useNavigate();
+
+        const submit = () => {
+            context.actions.signIn(emailAddress, password)
+                .then( user => {
+                    console.log(`${emailAddress} is now signed in.`);
+                    navigate(-1);
+                })
+                .catch ( error => {
+                    console.log(error);
+                    if( error.response ) {
+                        console.log(error.response);
+                        setErrors(["Incorrect Username or Password."]);
+                    } else {
+                        console.log(error.response)
+                        navigate('/error');
+                    }
+                });
+        }
+        
     return (
-        <main>
+        <React.Fragment>
             <div className="form--centered">
                 <h2>Sign In</h2>
 
-                <form>
-                    <label htmlFor="emailAddress">Email Address</label>
-                    <input
-                        id="emailAddress"
-                        name="emailAddress"
-                        type="email"
-                        value=""
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        value=""
-                    />
+                <Form
+                    cancel={cancel}
+                    errors={errors}
+                    submit={submit}
+                    submitButtonText="Sign In"
+                    elements={() => (
+                        <React.Fragment>
+                            <label>Email Address
+                                <input
+                                    id="emailAddress"
+                                    name="emailAddress"
+                                    type="email"
+                                    value={emailAddress}
+                                    onChange={e => setEmailAddress(e.target.value)}
+                                    placeholder="Email Address"
+                                />
+                            </label>
+                            <label>Password
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    placeholder="Password"
+
+                                />
+                            </label>
+                        </React.Fragment>
+                    )}/>
                     <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick={actions.cancelForm}>Cancel</button>
-                </form>
                 <p>Don't have a user account? Click here to <a href="/signup">sign up</a>!</p>
             </div>
-        </main>
+        </React.Fragment>
     )
 }
 
