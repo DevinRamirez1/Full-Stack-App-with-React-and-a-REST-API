d732209b8068f97ac701a763488034fdbf54fdee
DevinRamirez1 <devinramirez207@gmail.com>
2022-02-19 11:06:11 -0600

    Update CourseDetail.js
    Added Context

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,19 +1,52 @@
-import axios from 'axios';
-import { useEffect, useContext, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
-import
+import React, { useEffect, useState } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
-function CourseDetail() {
+function CourseDetail({ context }) {
+    const { id } = useParams();
+    const [ course, setCourse ] = useState({});
+    const [user, setUser] = useState({});
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        context.data.api(`/courses/${id}`)
+            .then(response => {
+                setCourse(response.data);
+                setUser(response.data.user);
+            })
+            .catch (error => {
+                if (error.response.status === 404) {
+                    navigate('/notfound');
+                } else {
+                    console.log(error);
+                    navigate('/error');
+                }
+            })
+    }, []);
+
     return (
-        <main>
             <React.Fragment>
                 <div className="actions--bar">
                     <div className="wrap">
-                    <React.Fragment>
-                        <a className="button" href={`/courses/${param.id}/update`}>Update Course</a>
-                        <a className="button" href={"/"}></a>
-                    </React.Fragment>
-                    <a className="button button--secondary" href={'/'}>Return to course list</a>
+                        {context.authenticatedUser ? 
+                            context.authenticatedUser.emailAddress === user.emailAddress
+                            ?
+                                <React.Fragment>
+                                    <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
+                                    <a className="button" href={"/"}
+                                        onClick={
+                                            (e) => context.data.deleteCourse(
+                                                id,
+                                                context.authenticatedUser.emailAddress,
+                                                context.authenticatedUser.password
+                                            )
+                                        }>Delete Course</a>
+                                </React.Fragment>
+                            : null
+                        :null
+                        }
+                        <Link className="button button--secondary" to={'/'}>Return to course list</Link>
                     </div>
                 </div>
 
@@ -24,20 +57,22 @@ function CourseDetail() {
                             <div>
                                 <h3 className="course--detail--title">Course</h3>
                                 <h4 className="course--name">{course.title}</h4>
-                                <p></p>
+                                <p>By: {user.firstName} {user.lastName}</p>
+                                <ReactMarkdown>{course.description}</ReactMarkdown>
                             </div>
                             <div>
                                 <h3 className="course--detail--title">Estimated Time</h3>
-                                <p></p>
+                                <p>{course.estimatedTime}</p>
 
                                 <h3 className="course--detail--title">Materials Needed</h3>
-                                <ul></ul>
+                                <ul className='course--detail--list'>
+                                    <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
+                                </ul>
                             </div>
                         </div>
                     </form>
                 </div>
             </React.Fragment>
-        </main>
     )
 }
 
