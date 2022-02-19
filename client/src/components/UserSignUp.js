56a436a84764dd33983af2d520ee3cad074594fe
DevinRamirez1 <devinramirez207@gmail.com>
2022-02-19 10:31:23 -0600

    Update UserSignUp.js
    Added context to file

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,41 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import Form from './Form';
+import axios from 'axios';
 
-function UserSignUp() {
+export default function UserSignUp({ context }) {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState('');
+
+    const navigate = useNavigate();
+
+    const submit = () => {
+        const user = {
+            firstName,
+            lastName,
+            emailAddress,
+            password,
+        };
+
+        context.data.createUser(user)
+            .then( () => {
+                console.log(`${firstName} is signed up.`);
+                context.actions.signIn(emailAddress, password)
+                    .then(() => {
+                        navigate('/');
+                    });
+                console.log(`${emailAddress} is signed up and authenticated.`)
+            })
+            .catch (error => {
+                if (error.response) {
+                    setErrors(error.response.data.errors)
+                } else {
+                    navigate('/error');
+                }
+            });
+    }
+
+    cancel = () => {
+        navigate('/');
+    }
+
     return (
-        <main>
             <div className="form--centered">
                 <h2>Sign Up</h2>
 
-                <form>
-                    <label htmlFor="firstName">First Name</label>
-                    <input
-                        id="firstName"
-                        name="firstName"
-                        type="text"
-                    />
-                    <label htmlFor="lastName">Last Name</label>
-                    <input
-                        id="lastName"
-                        name="lastName"
-                        type="text"
-                    />
-                    <label htmlFor="emailAddress">Email Address</label>
-                    <input
-                        id="emailAddress"
-                        name="emailAddress"
-                        type="email"
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                    />
-                    <button className="button" type="submit">Sign Up</button><button className="button button--secondary" onClick={actions.cancelForm}>Cancel</button>
-                </form>
+                <Form
+                    cancel={cancel}
+                    errors={errors}
+                    submit={submit}
+                    submitButtonText='Sign Up'
+                    elements={() => (
+                        <React.Fragment>
+                            <label> First Name
+                                <input
+                                    id="firstName"
+                                    name="firstName"
+                                    type="text"
+                                />
+                            </label>
+                            <label>Last Name
+                                <input
+                                    id="lastName"
+                                    name="lastName"
+                                    type="text"
+                                />
+                            </label>
+                            <label>Email Address
+                                <input
+                                    id="emailAddress"
+                                    name="emailAddress"
+                                    type="email"
+                                />
+                            </label>
+                            <label>Password
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                />
+                            </label>
+                        </React.Fragment>
+                    )}
+                />
                 <p>Already have a user account? Click here to <a href="/signup">sign up</a>!</p>
             </div>
-        </main>
     )
 }
 
