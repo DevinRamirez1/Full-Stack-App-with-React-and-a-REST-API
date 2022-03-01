32c1fd6b3488d8bedab91f6e5d5cac4fd05feff0
DevinRamirez1 <devinramirez207@gmail.com>
2022-02-28 18:17:37 -0600

    Update Data.js
    Added a getCourse function to data

--- client/src/Data.js
+++ client/src/Data.js
@@ -37,6 +37,17 @@ export default class Data {
             }
         }
 
+        async getCourse(id) {
+            const response = await this.api('/courses/${id}', 'GET', null);
+            if (response.status === 200) {
+                return response.json().then(data => data);
+            } else if (response.status === 404){
+                return response.status;
+            } else {
+                throw new Error();
+            }
+        }
+
         async createUser(user) {
             const response = await this.api('/users', 'POST', user);
             if (response.status === 201) {
