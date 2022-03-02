6826828636ae76e6a26f1078a9ccb47eeb1f7e61
DevinRamirez1 <devinramirez207@gmail.com>
2022-03-02 17:04:26 -0600

    Update UpdateCourse.js
    Typed out code for UpdateCourse

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,47 +1,127 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import Form from "./Form";
 
-function UpdateCourse() {
+export default function UpdateCourse({ context }) {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [estimatedTime, setEstimatedTime] = useState('');
+    const [materialsNeeded, setMaterialsNeeded] = useState('');
+    const [author, setAuthor] = useState('');
+    const [userId, setUserId] = useState('');
+    const [errors, setErrors] = useState('');
+
+    const { id } = useParams();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        context.data.api(`/courses/${id}`)
+            .then(response => {
+                if (response.course.userId === context.authenticatedUser.userId) {
+                    setTitle(response.data.title)
+                    setDescription(response.data.description)
+                    setEstimatedTime(response.data.estimatedTime)
+                    setMaterialsNeeded(response.data.materialsNeeded)
+                    setUserId(response.data.userId)
+                    setAuthor(`${response.data.User.firstName} ${response.data.User.lastName}`)
+                } else {
+                    navigate('/forbidden');
+                }
+            })
+            .catch (error => {
+                if (error.response.status === 404) {
+                    navigate('/notfound');
+                } else {
+                    navigate('/error')
+                }
+            })
+    }, [context, id, navigate]);
+
+    const submit = () => {
+        const course = {
+            title,
+            description,
+            estimatedTime,
+            materialsNeeded,
+            userId
+        };
+
+        const emailAddress = context.authenticatedUser.emailAddress;
+        const password = context.authenticatedUser.password;
+
+        context.data.updateCourse(id, course, emailAddress, password)
+            .then(() => {
+                console.log('Course has been updated.')
+                navigate(`/courses/${id}`)
+            })
+            .catch (error => {
+                if (error.response.status === 401) {
+                    navigate('/forbidden');
+                } else if (error.response) {
+                    setErrors(error.response.data.errors);
+                } else {
+                    navigate('/error')
+                }
+            });
+    }
+
+    const cancel = () => {
+        navigate('/');
+    }
+
     return(
-        <main>
+        <React.Fragment>
             <div className="wrap">
                 <h2>Update Course</h2>
-                <form>
-                    <div className="main--flex">
-                        <div>
-                            <label htmlFor="courseTitle">Course Title</label>
-                            <input
-                                id="courseTitle"
-                                name="courseTitle"
-                                type="text"
-                            />
+                <Form
+                    cancel={cancel}
+                    errors={errors}
+                    submit={submit}
+                    submitButtonText='Update Course'
+                    elements={() => (
+                        <div className="main--flex">
+                            <div>
+                                <input
+                                    id="courseTitle"
+                                    name="courseTitle"
+                                    type="text"
+                                    value={title}
+                                    onChange={e => setTitle(e.target.value)}
+                                    placeholder='Course Title'
+                                />
 
-                            <p>By</p>
-
-                            <label htmlFor="courseDescription">Course Description</label>
-                            <textarea
-                                id="courseDescription"
-                                name="courseDescription"
-                            />
+                                <p>By: {author}</p>
+                                <textarea
+                                    id="courseDescription"
+                                    name="courseDescription"
+                                    value={description}
+                                    onChange={e => setDescription(e.target.value)}
+                                    placeholder='Course Description'
+                                />
+                            </div>
+                            <div>
+                                <input
+                                    id="estimatedTime"
+                                    name="estimatedTime"
+                                    type="text"
+                                    value={estimatedTime}
+                                    onChange={e => setEstimatedTime(e.target.value)}
+                                    placeholder='Estimated Time'
+                                />
+                                <textarea
+                                    id="materialsNeeded"
+                                    name="materialsNeeded"
+                                    value={materialsNeeded}
+                                    onChange={e => setMaterialsNeeded(e.target.value)}
+                                    placeholder='Materials Needed'
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <label htmlFor="estimatedTime">Estimated Time</label>
-                            <input
-                                id="estimatedTime"
-                                name="estimatedTime"
-                                type="text"
-                            />
-
-                            <label htmlFor="materialsNeeded">Materials Needed</label>
-                            <textarea
-                                id="materialsNeeded"
-                                name="materialsNeeded"
-                            />
-                        </div>
-                    </div>
-                </form>
+                    )}
+                />
             </div>
-        </main>
+        </React.Fragment>
     )
 }
-
-export default UpdateCourse;
